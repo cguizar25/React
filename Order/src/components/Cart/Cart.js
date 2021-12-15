fd1dce73bb641078a99de0681f0afecf163c6c94
Christian Guizar <c-guizar@hotmail.com>
2021-12-14 16:42:32 -0800

    feature(food order app): integrated a more interactive cart

--- Order/src/components/Cart/Cart.js
+++ Order/src/components/Cart/Cart.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
+import cartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+  const cartCtx = useContext(cartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
 
   const cartItems = (
     <ui className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => (
-        <li>{item.name}</li>
+      {cartCtx.items.map(item => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          />
       ))}
     </ui>
   );
@@ -19,11 +40,11 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onClose} className={classes['button--alt']}>Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   )
