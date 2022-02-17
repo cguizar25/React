292f997cdf4c87ee1de82151e02f66a517325280
Christian Guizar <c-guizar@hotmail.com>
2022-02-16 16:19:28 -0800

    merge

---
+++ Authentication/src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import { AuthContextProvider } from './store/auth-context';
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>,
+  document.getElementById('root')
+
+);
--- Quotes/src/App.js
+++ Quotes/src/App.js
@@ -1,15 +1,18 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
   return (
-    <div>
-      <Layout>
+    <Layout>
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
         <Switch>
           <Route path='/' exact>
             <Redirect to='/quotes' />
@@ -27,8 +30,8 @@ function App() {
             <NotFound />
           </Route>
         </Switch>
-      </Layout>
-    </div>
+      </Suspense>
+    </Layout>
   );
 }
 
